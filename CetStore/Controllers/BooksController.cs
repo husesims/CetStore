8e0879491e3a9782dfe316e375a3864c9b42ca56
Huseyin Simsek <huseyin.simsek@boun.edu.tr>
2019-02-28 13:28:31 +0300

    Add project files.

---
+++ CetStore/Controllers/BooksController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CetStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CetStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        List<Book> books = new List<Book>();
+        public BooksController()
+        {
+            Book book1 = new Book();
+            book1.Id = 1;
+            book1.Name = "CET 301";
+            book1.Price = 99.99M;
+            books.Add(book1);
+            Book book2 = new Book();
+            book2.Id = 2;
+            book2.Name = "C# ve Veritabanı";
+            book2.Price = 199.99M;
+            books.Add(book2);
+
+        }
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+
+        public IActionResult Detay(int id)
+        {
+            Book book = new Book();
+            if (id == 1)
+            {
+                book = books[0];
+            } else if (id==2) {
+                book = books[1];
+            } else
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+    }
+}
---
+++ CetStore/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
