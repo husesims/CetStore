d831e25018cb228fe3972c63a1677302e34fdbac
Huseyin Simsek <huseyin.simsek@boun.edu.tr>
2019-02-28 13:58:08 +0300

    connection eklendi

---
+++ CetStore/Models/CetStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetStore.Models
+{
+    public class CetStoreContext : DbContext
+    {
+        public CetStoreContext(DbContextOptions<CetStoreContext> options):base(options)
+        {
+
+        }
+       public DbSet<Book> Books { get; set; }
+    }
+}
--- CetStore/Startup.cs
+++ CetStore/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CetStore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,6 +34,10 @@ namespace CetStore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            
+            services.AddDbContext<CetStoreContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("CetStoreConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
